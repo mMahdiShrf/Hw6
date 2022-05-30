b92d99dc75569d28cde4cce42081b6f2460624d1
mMahdiShrf <mahdi.sahrif20001@gmail.com>
2022-05-30 16:22:36 +0000

    answer to question 1

--- include/q1.h
+++ include/q1.h
@@ -1,4 +1,37 @@
 #ifndef Q1_H
 #define Q1_H
 
+#include<iostream>
+#include<cmath>
+#include <limits>
+
+
+namespace q1
+{
+    template<typename T, typename Func>
+    T derivate(T point,Func func)
+    {
+        return (func(point+ 0.001) - func(point - 0.001))/0.002;
+    }
+
+    template<typename T, typename Func>
+    T gradient_descent(T inital_value,T step_size, Func func=Func{})
+    {   
+        T second_value{};
+        T first_value { inital_value };
+        bool flag{true};
+        do 
+        {   
+            second_value = first_value - step_size*derivate(first_value,func);
+            if (std::abs(second_value - first_value) > 1e-50)
+                first_value = second_value;
+            else
+                flag = false;
+        }
+        while(flag);
+        return second_value;
+    }
+}
+
+
 #endif //Q1_H
