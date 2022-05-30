7c732822cee55b571252ddfa1c4fb5b0f75c3c8a
mMahdiShrf <mahdi.sahrif20001@gmail.com>
2022-05-30 18:10:12 +0000

    implement Patient class

--- include/q2.h
+++ include/q2.h
@@ -1,4 +1,28 @@
 #ifndef Q2_H
 #define Q2_H
 
+#include<vector>
+#include<iostream>
+#include<sstream>
+#include<fstream>
+
+namespace q2
+{
+    struct Patient
+    {
+        Patient(std::string _name,size_t _age,size_t _smokes,size_t _area_q, size_t _alkhol)
+        :name{_name}
+        ,age{_age}
+        ,somokes{_smokes}
+        ,area_q{_area_q}
+        ,alkhol{_alkhol}
+        {}
+        std::string name;
+        size_t age;
+        size_t smokes;
+        size_t area_q;
+        size_t alkhol;
+    };
+}
+
 #endif //Q2_H
