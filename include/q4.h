a887c6b4af593da3d785b968c6b7cb2e4c3ecee9
mMahdiShrf <mahdi.sahrif20001@gmail.com>
2022-05-31 19:39:44 +0000

    implement kalman_filter

--- include/q4.h
+++ include/q4.h
@@ -2,7 +2,9 @@
 #define Q4_H
 
 #include<iostream>
-
+#include<vector>
+#include<queue>
+#include<numeric>
 
 namespace q4
 {
@@ -15,7 +17,7 @@ namespace q4
         double y{}; 
     };
 
-struct Sensor
+    struct Sensor
     {
         Sensor(Vector2D _pos, double _accuracy)
         :pos{_pos}
@@ -23,6 +25,17 @@ struct Sensor
         Vector2D pos;
         double accuracy;    
     };
+    Vector2D kalman_filter(std::vector<Sensor> sensors)
+    {
+        double sum_x{};
+        double sum_y{};
+        double weights{};
+        sum_x  = std::accumulate(sensors.begin(),sensors.end(),sum_x,[](double sum_result,Sensor s){return sum_result+s.pos.x*s.accuracy;});
+        sum_y = std::accumulate(sensors.begin(),sensors.end(),sum_y,[](double sum_result,Sensor s){return sum_result+s.pos.y*s.accuracy;});
+        weights=  std::accumulate(sensors.begin(),sensors.end(),weights,[](double sum_result,Sensor s){return sum_result+s.accuracy;});
+        return Vector2D{sum_x/weights,sum_y/weights};
+    }
+
 }
 
 #endif //Q4_H
