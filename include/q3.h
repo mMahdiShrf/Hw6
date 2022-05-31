b6e9be040add0f656953776ddb2559f839360edf
mMahdiShrf <mahdi.sahrif20001@gmail.com>
2022-05-31 13:30:07 +0000

    implement gather_flights

--- include/q3.h
+++ include/q3.h
@@ -7,12 +7,13 @@
 #include<sstream>
 #include<regex>
 #include<vector>
+#include<queue>
 
 
 namespace q3
 {
-struct Flight
-{
+    struct Flight
+    {
     Flight(std::string _flight_number, size_t _duration, size_t _connections, size_t _connection_times, size_t _price)
     :flight_number{_flight_number}
     ,duration{_duration}
@@ -25,7 +26,61 @@ struct Flight
 	size_t connections;
 	size_t connection_times;
 	size_t price;
+    };
+
+    size_t time2minute(std::string time)
+    {
+        if(time.empty())
+            return 0;
+        std::regex pattern(R"((\d+)h(\d+)?m?)");
+        std::smatch match;
+        size_t minute{};
+        std::regex_search(time, match,pattern);
+        minute += static_cast<size_t>(std::stoi(match[1]))*60;
+        std::string sth {match[2]};
+        if(sth.empty())
+            return minute;
+        else
+            return minute + static_cast<size_t>(std::stoi(match[2]));
+    }
+
+    auto comparison {[](Flight f1, Flight f2){
+        return (f1.duration+f1.connection_times+ 3*f1.price) >
+        (f2.duration+f2.connection_times+ 3*f2.price);
+    }};
+
+    auto gather_flights(std::string filename)
+    {
+
+        std::priority_queue<Flight,std::vector<Flight>,decltype(comparison)> flights{comparison};
+        std::ifstream file(filename);
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string txt = buffer.str();
+        std::regex pattern(R"(\d+\-\ flight_number:(\w+)\ \-\ duration:(\w+)\ \-\ connections:(\d+)\ \-\ connection_times:(\w+\,?)(\w+\,?)?(\w+\,?)?\ \-\ price:(\d+))");
+        std::smatch match;
+        while(std::regex_search(txt, match, pattern))
+        {   
+            std::string flight_number{match[1]};
+            size_t duration{time2minute(match[2])};
+            size_t connections{static_cast<size_t>(std::stoi(match[3]))};
+            size_t connections_times{};
+            connections_times +=  time2minute(match[4]);
+            connections_times +=  time2minute(match[5]);
+            connections_times +=  time2minute(match[6]);
+            size_t price{static_cast<size_t>(std::stoi(match[7]))};
+            std::cout << connections_times << std::endl;
+            flights.push(Flight{flight_number,duration,connections,connections_times,price});
+            txt = match.suffix().str();
+        }
+
+
+        return flights;
+    }
+    
+
 };
-}
+
+    
 
 #endif //Q3_H
